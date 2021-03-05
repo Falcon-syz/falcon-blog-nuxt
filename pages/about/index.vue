a26369c719dc5417d960637e0d5d9a33142c8ed3
Falcon-syz <sunyzh9527@sina.com>
2021-03-05 15:22:04 +0800

    sunyzh

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,32 +1,44 @@
 <template>
   <div class="container">
-      <a-skeleton :paragraph="{ rows: 20 }" active v-show="isShowSkeleton" />
-      开发中
+    <div class="box">
+     <img src="@/assets/img/userImg/v2-f3731d0b549a0ccfe84b3419b64eaa67_720w.jpg" class="about-avatar">
+     <p>
+       前端程序员
+     </p>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  layout(context) {
+    return "light";
+  },
   components: {},
   props: {},
   data() {
-    return {
-      isShowSkeleton: false,
-    };
+    return {};
   },
   computed: {},
   watch: {},
   created() {},
-  mounted() {
-    this.isShowSkeleton = true;
-    setTimeout(() => {
-      this.isShowSkeleton = false;
-    }, 500);
-  },
+  mounted() {},
   updated() {},
   methods: {},
 };
 </script>
 
 <style scoped>
+.about-avatar{
+  width: 120px;
+  height:120px;
+  border-radius: 90px;
+  margin-top: 40px;
+}
+.box {
+  text-align: center;
+  width: 100%;
+  min-height: 800px;
+}
+
 </style>
